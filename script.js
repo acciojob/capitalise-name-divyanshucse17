60ae0f3b2fc6dc9e6e86db10f4fb190e9d704c7b
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-14 09:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+// Get the input field by its id
+const inputField = document.getElementById("fname");
+
+// Add blur event listener (fires when input loses focus)
+inputField.addEventListener("blur", function () {
+  this.value = this.value.toUpperCase();
+});
